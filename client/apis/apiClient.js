77a9aca3a56672061ad61d14b3d5f9ba18357ee5
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-11-19 21:35:06 +1300

    add apiClient for boards

--- client/apis/apiClient.js
+++ client/apis/apiClient.js
@@ -1,7 +1,10 @@
 import request from 'superagent'
 
-const rootUrl = '/api/v1'
+const rootUrl = '/api/v1/'
+const boardRootUrl = '/api/v1/board/'
+const userRootUrl = '/api/v1/profile/'
 
+// --- external API ---
 //get list of movies that matches the searched keyword
 export function getMovieData(movieTitle) {
   return request.get(`${rootUrl}/movies/${movieTitle}`).then((response) => {
@@ -9,3 +12,44 @@ export function getMovieData(movieTitle) {
     return response.body.results
   })
 }
+
+// --- board api ---
+//GET all baords
+export function getAllBoards() {
+  return request.get(boardRootUrl).then((res) => {
+    return res.body
+  })
+}
+
+//GET board by ID
+export function getBoardById(id) {
+  return request.get(`${boardRootUrl}${id}`).then((res) => {
+    return res.body
+  })
+}
+
+//GET board by status
+export function getBoardByStatus(status) {
+  return request.get(`${boardRootUrl}${status}`).then((res) => {
+    return res.body
+  })
+}
+
+//UPDATE editing the baord
+export function editBoard(id, newInfo) {
+  return request
+    .patch(`${boardRootUrl}${id}`)
+    .send(newInfo)
+    .then((res) => {
+      return res.body
+    })
+}
+
+//DELETE existing board
+export function deleteBoardById(id) {
+  return request.delete(`${boardRootUrl}${id}`).then((res) => {
+    return res.body
+  })
+}
+
+// --- userProfile api ---
