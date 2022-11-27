9bf4ec48f42fd14cba7075552444ac044960bb25
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-11-28 08:55:02 +1300

    change external api and update the data example.

--- client/apis/apiClient.js
+++ client/apis/apiClient.js
@@ -6,7 +6,6 @@ const rootUrl = '/api/v1'
 //get list of movies that matches the searched keyword
 export function getMovieData(movieTitle) {
   return request.get(`${rootUrl}/movies/${movieTitle}`).then((response) => {
-    // console.log(response.body.Search)
     return response.body.Search
   })
 }
